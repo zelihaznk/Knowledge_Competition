9abcf683d42e9197aed87bc8db40af5d6c2409fa
Zeliha Oznuk <zeliha25@gmail.com>
2022-12-25 01:26:24 +0200

    Add project files.

---
+++ Knowledge_Competition/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Knowledge_Competition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnA = new System.Windows.Forms.Button();
+            this.BtnB = new System.Windows.Forms.Button();
+            this.BtnC = new System.Windows.Forms.Button();
+            this.BtnD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblQuestion = new System.Windows.Forms.Label();
+            this.LblTrue = new System.Windows.Forms.Label();
+            this.LblFalse = new System.Windows.Forms.Label();
+            this.BtnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(453, 270);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnA
+            // 
+            this.BtnA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnA.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnA.Location = new System.Drawing.Point(12, 301);
+            this.BtnA.Name = "BtnA";
+            this.BtnA.Size = new System.Drawing.Size(218, 42);
+            this.BtnA.TabIndex = 1;
+            this.BtnA.Text = "A";
+            this.BtnA.UseVisualStyleBackColor = false;
+            this.BtnA.Click += new System.EventHandler(this.BtnA_Click);
+            // 
+            // BtnB
+            // 
+            this.BtnB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnB.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnB.Location = new System.Drawing.Point(247, 301);
+            this.BtnB.Name = "BtnB";
+            this.BtnB.Size = new System.Drawing.Size(218, 42);
+            this.BtnB.TabIndex = 2;
+            this.BtnB.Text = "B";
+            this.BtnB.UseVisualStyleBackColor = false;
+            this.BtnB.Click += new System.EventHandler(this.BtnB_Click);
+            // 
+            // BtnC
+            // 
+            this.BtnC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnC.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnC.Location = new System.Drawing.Point(12, 371);
+            this.BtnC.Name = "BtnC";
+            this.BtnC.Size = new System.Drawing.Size(218, 42);
+            this.BtnC.TabIndex = 3;
+            this.BtnC.Text = "C";
+            this.BtnC.UseVisualStyleBackColor = false;
+            this.BtnC.Click += new System.EventHandler(this.BtnC_Click);
+            // 
+            // BtnD
+            // 
+            this.BtnD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnD.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnD.Location = new System.Drawing.Point(247, 371);
+            this.BtnD.Name = "BtnD";
+            this.BtnD.Size = new System.Drawing.Size(218, 42);
+            this.BtnD.TabIndex = 4;
+            this.BtnD.Text = "D";
+            this.BtnD.UseVisualStyleBackColor = false;
+            this.BtnD.Click += new System.EventHandler(this.BtnD_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(540, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Question No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label2.Location = new System.Drawing.Point(611, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "True:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label3.Location = new System.Drawing.Point(606, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "False:";
+            // 
+            // LblQuestion
+            // 
+            this.LblQuestion.AutoSize = true;
+            this.LblQuestion.Font = new System.Drawing.Font("Corbel", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblQuestion.Location = new System.Drawing.Point(681, 68);
+            this.LblQuestion.Name = "LblQuestion";
+            this.LblQuestion.Size = new System.Drawing.Size(19, 22);
+            this.LblQuestion.TabIndex = 8;
+            this.LblQuestion.Text = "0";
+            // 
+            // LblTrue
+            // 
+            this.LblTrue.AutoSize = true;
+            this.LblTrue.Font = new System.Drawing.Font("Corbel", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblTrue.Location = new System.Drawing.Point(681, 108);
+            this.LblTrue.Name = "LblTrue";
+            this.LblTrue.Size = new System.Drawing.Size(19, 22);
+            this.LblTrue.TabIndex = 9;
+            this.LblTrue.Text = "0";
+            // 
+            // LblFalse
+            // 
+            this.LblFalse.AutoSize = true;
+            this.LblFalse.Font = new System.Drawing.Font("Corbel", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblFalse.Location = new System.Drawing.Point(681, 149);
+            this.LblFalse.Name = "LblFalse";
+            this.LblFalse.Size = new System.Drawing.Size(19, 22);
+            this.LblFalse.TabIndex = 10;
+            this.LblFalse.Text = "0";
+            // 
+            // BtnNext
+            // 
+            this.BtnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnNext.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnNext.Location = new System.Drawing.Point(524, 192);
+            this.BtnNext.Name = "BtnNext";
+            this.BtnNext.Size = new System.Drawing.Size(218, 42);
+            this.BtnNext.TabIndex = 11;
+            this.BtnNext.Text = "Next";
+            this.BtnNext.UseVisualStyleBackColor = false;
+            this.BtnNext.Click += new System.EventHandler(this.BtnNext_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(524, 310);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(657, 310);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(91, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(764, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 21);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(765, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 21);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(767, 435);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnNext);
+            this.Controls.Add(this.LblFalse);
+            this.Controls.Add(this.LblTrue);
+            this.Controls.Add(this.LblQuestion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnD);
+            this.Controls.Add(this.BtnC);
+            this.Controls.Add(this.BtnB);
+            this.Controls.Add(this.BtnA);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Knowledge Competition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnA;
+        private System.Windows.Forms.Button BtnB;
+        private System.Windows.Forms.Button BtnC;
+        private System.Windows.Forms.Button BtnD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblTrue;
+        private System.Windows.Forms.Label LblFalse;
+        private System.Windows.Forms.Button BtnNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label LblQuestion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Knowledge_Competition/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Knowledge_Competition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int questionNo = 0, dogru = 0, yanlis = 0;
+
+
+        private void BtnA_Click(object sender, EventArgs e)
+        {
+
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnNext.Enabled = true;
+
+            label5.Text = BtnA.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+
+            }
+            else
+            {
+                yanlis++;
+                LblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void BtnB_Click(object sender, EventArgs e)
+        {
+
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnNext.Enabled = true;
+
+            label5.Text = BtnB.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+
+            }
+            else
+            {
+                yanlis++;
+                LblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+
+
+        }
+
+        private void BtnC_Click(object sender, EventArgs e)
+        {
+
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnNext.Enabled = true;
+
+            label5.Text = BtnC.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+
+            }
+            else
+            {
+                yanlis++;
+                LblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+
+        }
+
+        private void BtnD_Click(object sender, EventArgs e)
+        {
+
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnNext.Enabled = true;
+
+            label5.Text = BtnD.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblTrue.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+
+            }
+            else
+            {
+                yanlis++;
+                LblFalse.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = true;
+            BtnB.Enabled = true;
+            BtnC.Enabled = true;
+            BtnD.Enabled = true;
+            BtnNext.Enabled = false;
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+
+            questionNo++;
+            LblQuestion.Text = questionNo.ToString();
+
+            if (questionNo == 1)
+            {
+                richTextBox1.Text = "Cumhuriyet kac yilinda ilan edilmistir?";
+                BtnA.Text = "1920";
+                BtnB.Text = "1921";
+                BtnC.Text = "1922";
+                BtnD.Text = "1923";
+                label4.Text = "1923";
+            }
+
+            if (questionNo == 2)
+            {
+                richTextBox1.Text = "Hangi il Ege Bolgemizde bulunmaz?";
+                BtnA.Text = "Izmir";
+                BtnB.Text = "Balikesir";
+                BtnC.Text = "Aydin";
+                BtnD.Text = "Manisa";
+                label4.Text = "Balikesir";
+            }
+
+            if (questionNo == 3)
+            {
+                richTextBox1.Text = "Son Kuşlar hangi yazarimiza aittir?";
+                BtnA.Text = "Sait Faik";
+                BtnB.Text = "Cemal Süreyya";
+                BtnC.Text = "Attila İlhan";
+                BtnD.Text = "Reşat Nuri";
+                label4.Text = "Sait Faik";
+            }
+
+            if (questionNo == 4)
+            {
+                richTextBox1.Text = "Java’da bir dizi bildirmek için hangisi kullanılır?";
+                BtnA.Text = " {} ";
+                BtnB.Text = " [] ";
+                BtnC.Text = " () ";
+                BtnD.Text = " <> ";
+                label4.Text = " [] ";
+            }
+
+            if (questionNo == 5)
+            {
+                richTextBox1.Text = "Aşağıdakilerden hangisi tip günveliğini garanti eder?";
+                BtnA.Text = "Generics";
+                BtnB.Text = "Abstract classes";
+                BtnC.Text = "Interfaces";
+                BtnD.Text = "Collection";
+                label4.Text = "Generics";
+            }
+
+            if (questionNo == 6)
+            {
+                richTextBox1.Text = "Kodun ekran çıktısı nedir?" +
+                    "\r\nimport java.util.HashMap;\r\nimport java.util.Map;\r\nclass Main {\r\n  public static void main(String args[])\r\n  {\r\n    Map hashMap = new HashMap();\r\n    hashMap.put(new String(\"a\"),\"audi\");\r\n    hashMap.put(new String(\"a\"),\"ferrari\");\r\n    System.out.println(hashMap);\r\n  }\r\n}\r\n ";
+                BtnA.Text = "Runtime exception";
+                BtnB.Text = " [1] ";
+                BtnC.Text = " [null, 1] ";
+                BtnD.Text = " {a=ferrari} ";
+                label4.Text = " {a=ferrari} ";
+            }
+
+            if (questionNo == 7)
+            {
+                richTextBox1.Text = "Aşağıdaki kodun ekran çıktısı nedir?" +
+                    "\r\nimport java.util.ArrayList;\r\nimport java.util. Iterator;\r\nimport java.util. List;\r\nclass Main {\r\n  public static void main(String args[])\r\n  {\r\n    List arrayList = new ArrayList();\r\n \r\n    arrayList.add(\"a\");\r\n    arrayList.add(\"b\");\r\n \r\n    Iterator iterator = arrayList.iterator();\r\n \r\n    while (iterator.hasNext())\r\n    {\r\n    System.out.println(iterator.next());\r\n    arrayList.add(\"c\");\r\n    }\r\n  }\r\n} ";
+                BtnA.Text = "a ardından ConcurrentModificationException";
+                BtnB.Text = "a b c";
+                BtnC.Text = "a b";
+                BtnD.Text = "a c";
+                label4.Text = "a ardından ConcurrentModificationException";
+
+            }
+
+            if (questionNo == 8)
+            {
+                richTextBox1.Text = "C# hakkında aşağıdakilerden hangisi doğrudur ?";
+                BtnA.Text = "Çeşitli bilgisayar platformlarında derlenebilir.";
+                BtnB.Text = ".Net Framework'ün bir parçasıdır.";
+                BtnC.Text = "Bileşen yönelimlidir (component oriented).";
+                BtnD.Text = "Hepsi";
+                label4.Text = "Hepsi";
+            }
+
+            if (questionNo == 9)
+            {
+                richTextBox1.Text = "X ve Y değişkenlerine ilişkin değer atama hangisinde doğru yapılmıştır ?";
+                BtnA.Text = "int X = Y = 40;";
+                BtnB.Text = "int X = 35; int Y = 40;";
+                BtnC.Text = "int X = 45; Y = 140;";
+                BtnD.Text = "int X = 32, Y = 16.6;";
+                label4.Text = "int X = 35; int Y = 40;";
+            }
+
+            if (questionNo == 10)
+            {
+                richTextBox1.Text = "Uzun veri türü olarak C#.Net içinde kaç Bayt saklanır ?";
+                BtnA.Text = "8";
+                BtnB.Text = "4";
+                BtnC.Text = "2";
+                BtnD.Text = "1";
+                label4.Text = "8";
+
+                
+            }
+
+
+            if (questionNo == 11)
+            {
+
+                BtnA.Enabled = false;
+                BtnB.Enabled = false;
+                BtnC.Enabled = false;
+                BtnD.Enabled = false;
+
+                BtnNext.Enabled = false;
+
+                BtnNext.Text = "Results";
+
+                MessageBox.Show("True:" + dogru +"\n" + "False:" + yanlis);
+                
+            }
+
+
+        }
+    }
+}
